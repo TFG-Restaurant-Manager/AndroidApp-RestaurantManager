ef43c6e1d9779b61d35f289aff9acc8ec52595b5
Aitor de Santos Amoros <soyadsa06@gmail.com>
2025-12-22 13:08:34 +0100

    Test commit

--- app/src/main/java/com/tfg_rm/androidapp_restaurantmanager/MainActivity.kt
+++ app/src/main/java/com/tfg_rm/androidapp_restaurantmanager/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidAppRestaurantManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Text("Text")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidAppRestaurantManagerTheme {
-        Greeting("Android")
     }
 }
