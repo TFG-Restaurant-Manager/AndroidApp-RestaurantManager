3842db5e99081d2b0a8a7cc3f566a691e199498b
diego123732 <diegocrack345678@gmail.com>
2026-01-08 23:15:20 +0100

    I have completed nearly all the ui needed for the food screen but now, I don't know why, it's starting to crash, the last change that I have done is adding the text of search to the filter of food

--- app/src/main/java/com/tfg_rm/androidapp_restaurantmanager/ui/screens/FoodScreen.kt
+++ app/src/main/java/com/tfg_rm/androidapp_restaurantmanager/ui/screens/FoodScreen.kt
@@ -1,40 +1,109 @@
 package com.tfg_rm.androidapp_restaurantmanager.ui.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.rememberLifecycleOwner
-import com.tfg_rm.androidapp_restaurantmanager.navigation.AppScreens
+import com.tfg_rm.androidapp_restaurantmanager.data.models.Dishes
+import com.tfg_rm.androidapp_restaurantmanager.data.models.Tables
+import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun FoodScreen () {
-    Row (
+    val tableExample : Tables = Tables(3, 4, 1,
+        10, 10, true)
+    val dishesExamples : List<Dishes> = listOf(
+        Dishes(1, "Puré de patata", "Patatas cocidas con varios condimentos", "Principales", 8.5, true),
+        Dishes(2, "Ensalada César", "Lechuga romana con pollo, crutones y salsa césar", "Entrantes", 7.5, true),
+        Dishes(3, "Croquetas caseras", "Croquetas cremosas de jamón", "Entrantes", 6.0, true),
+        Dishes(4, "Carpaccio de ternera", "Finas láminas de ternera con parmesano y aceite de oliva", "Entrantes", 9.0, true),
+        Dishes(5, "Solomillo al Roquefort", "Solomillo de ternera con salsa de queso Roquefort", "Principales", 18.5, true),
+        Dishes(6, "Lubina a la sal", "Lubina horneada con costra de sal", "Principales", 17.0, true),
+        Dishes(7, "Paella valenciana", "Paella tradicional de pollo y verduras", "Principales", 15.0, true),
+        Dishes(8, "Risotto de setas", "Arroz cremoso con setas y parmesano", "Principales", 14.0, true),
+        Dishes(9, "Tarta de queso", "Tarta cremosa de queso al horno", "Postres", 5.5, true),
+        Dishes(10, "Tiramisú", "Postre italiano con café y cacao", "Postres", 6.0, true),
+        Dishes(11, "Coulant de chocolate", "Bizcocho de chocolate con corazón fundente", "Postres", 6.5, true),
+        Dishes(12, "Agua mineral", "Botella de agua mineral", "Bebidas", 2.0, true),
+        Dishes(13, "Vino tinto crianza", "Copa de vino tinto crianza", "Bebidas", 4.5, true),
+        Dishes(14, "Cerveza", "Cerveza fría", "Bebidas", 3.0, true)
+    )
+    val dishesCategories = dishesExamples.map { it.category }
+        .distinct().let { listOf("Todo") + it }
+    var selectedCategory by remember { mutableStateOf(dishesCategories[0]) }
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        TopTableBar("Mesa ${tableExample.id}")
+
+        var searchedDishe by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = searchedDishe,
+            onValueChange = { searchedDishe = it },
+            placeholder = { Text("Buscar producto...") },
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = null)
+            },
+            singleLine = true,
+            shape = RoundedCornerShape(50),
+            modifier = Modifier.padding(horizontal = 10.dp).fillMaxWidth()
+        )
+        Spacer(Modifier.padding(5.dp))
+        CategorySelector(dishesCategories,
+            selectedCategory,
+            onCategorySelected = {selectedCategory = it}
+            )
+
+        DishesList(dishesExamples,searchedDishe, selectedCategory)
+    }
+}
+
+@Composable
+fun TopTableBar (tableName: String) {
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(14.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+            .height(56.dp)
     ) {
-        Button(
-            onClick = {}
+        IconButton (
+            onClick = {},
+            modifier = Modifier.align(Alignment.CenterStart)
         ) {
             Icon(
                 imageVector = Icons.AutoMirrored.Filled.ArrowBack,
@@ -43,8 +112,89 @@ fun FoodScreen () {
                 modifier = Modifier.size(14.dp)
             )
         }
+        Text(tableName,
+            modifier = Modifier.align(Alignment.Center))
+    }
+}
 
-        Text("Mesa 3")
-        Text("")
+@Composable
+fun CategorySelector(
+    categories: List<String>,
+    selectedCategory: String,
+    onCategorySelected: (String) -> Unit
+) {
+    HorizontalDivider(
+        thickness = 1.dp,
+        color = Color.LightGray
+    )
+    LazyRow(
+        modifier = Modifier.padding(vertical = 7.dp).fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(6.dp),
+        contentPadding = PaddingValues(horizontal = 14.dp)
+    ) {
+        items(categories) { category ->
+            Button(
+                onClick = { onCategorySelected(category) },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = if (selectedCategory == category) Color(0xFFF59E0B) else Color.White,
+                    contentColor = if (selectedCategory == category) Color.White else Color.DarkGray
+                ),
+                shape = RoundedCornerShape(6.dp),
+                border = if (selectedCategory != category) BorderStroke(1.dp, Color.LightGray) else null
+            ) {
+                Text(category)
+            }
+        }
+    }
+
+    HorizontalDivider(
+        thickness = 1.dp,
+        color = Color.LightGray
+    )
+}
+
+@Composable
+fun DishesList (dishes : List<Dishes>,
+                searchedDishe : String,
+                selectedCategory: String) {
+    val filteredDishes = dishes.filter { dish ->
+        val matchesCategory =
+            selectedCategory == "Todo" || dish.category == selectedCategory
+
+        val matchesSearch =
+            searchedDishe.isBlank() ||
+                    dish.name.contains(searchedDishe, ignoreCase = true)
+
+        matchesCategory && matchesSearch
+    }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items( filteredDishes ) { dish ->
+            Card(
+                modifier = Modifier.padding(12.dp).fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(12.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(
+
+                    ) {
+                        Text(dish.name)
+                        Text(String.format(Locale.getDefault(), "%.2f €", dish.price))
+
+                    }
+                    Button(
+                        onClick = {}
+                    ) {
+                        Text("Agregar")
+                    }
+                }
+            }
+        }
     }
 }
